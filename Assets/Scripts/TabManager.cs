5af10a69372f810b4e748c13a10527b914c69482
Siddhant Bansal <bansal.siddhant@gmail.com>
2015-05-29 08:37:53 +0530

    + Added tabs and fonts * Need to fix tab script

---
+++ Assets/Scripts/TabManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TabManager : MonoBehaviour {
+
+	public GameObject panelsParent;
+
+	void Start () {		
+		SwitchMap ();
+	}
+
+	void DeactivateAll() {
+		foreach (Transform child in panelsParent.transform) {
+			child.gameObject.SetActive(false);
+		}
+	}
+
+	void Activate(string name) {
+		foreach (Transform child in panelsParent.transform) {
+			if(child.name == name) {
+				child.gameObject.SetActive(true);
+			}
+		}
+	}
+
+	public void SwitchMap() {
+		DeactivateAll ();
+		Activate ("MapPanel");	
+	}
+
+	public void SwitchQuests() {
+		DeactivateAll ();
+		Activate ("QuestsPanel");
+	}
+
+	public void SwitchSkills() {
+		DeactivateAll ();
+		Activate ("SkillsPanel");
+	}
+
+}
